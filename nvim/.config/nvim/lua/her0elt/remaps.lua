bf71029dd526833d61ed01d08483cd5d058e930e
her0elt <hermann.owren.elton@hotmail.com>
2022-12-03 11:24:09 +0100

    clipboard yank

--- nvim/.config/nvim/lua/her0elt/remaps.lua
+++ nvim/.config/nvim/lua/her0elt/remaps.lua
@@ -1,6 +1,17 @@
 local keymap = require("her0elt.keymap")
 
 local nnoremap = keymap.nnoremap;
+local vnoremap = keymap.vnoremap;
 
 vim.g.mapleader = " "
 nnoremap("<leader>pv", "<cmd>Ex<CR>")
+
+vnoremap("J", ":m '>+1<CR>gv=gv")
+vnoremap("K", ":m '<-2<CR>gv=gv")
+
+nnoremap("Y", "yg$")
+nnoremap("J", "mzJ`z")
+vnoremap("<leader>y", "\"+y")
+nnoremap("<leader>Y",  "\"+yg_")
+nnoremap("<leader>y",  "\"+y")
+nnoremap("<leader>yy",  "\"+yy")
