f5cd8b72c8e769ea3944e82419e121bb351e9237
her0elt <hermann.owren.elton@hotmail.com>
2023-09-18 12:45:49 +0200

    fix: clangd

--- nvim/.config/nvim/after/plugin/lsp.lua
+++ nvim/.config/nvim/after/plugin/lsp.lua
@@ -20,7 +20,6 @@ lsp.configure('sumneko_lua', {
     }
 })
 
-
 local cmp = require('cmp')
 local cmp_select = {behavior = cmp.SelectBehavior.Select}
 local cmp_mappings = lsp.defaults.cmp_mappings({
@@ -68,3 +67,12 @@ lsp.setup()
 vim.diagnostic.config({
     virtual_text = true,
 })
+
+
+require('lspconfig').clangd.setup{
+    capabilities = lsp.capabilities,
+    on_attach = function (client, bufnr)
+        client.server_capabilities.signatureHelpProvider = false
+        lsp.on_attach(client, bufnr)
+    end
+}
